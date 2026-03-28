678ee034017548c50efe32fc040840dcb18dd3a3
sawan chavhan <sawanchavhan2005@gmail.com>
2026-03-28 19:25:35 +0530

    Add files via upload

---
+++ Assignment_20.java
@@ -0,0 +1,11 @@
+
+// Q20. Use this keyword in constructor to initialize variables
+class EmployeeThis {
+    String name;
+    double salary;
+
+    EmployeeThis(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
